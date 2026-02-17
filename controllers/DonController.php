f9a9735aaf12ff1efe31912d450dedb65c887c90
HeryNyAina454 <andriahery32@gmail.com>
2026-02-17 10:40:20 +0300

    Update DonController.php

--- controllers/DonController.php
+++ controllers/DonController.php
@@ -11,20 +11,40 @@ class DonController {
         $db = (new Database())->getConnection();
         $donModel = new Don($db);
 
-        // On récupère les 4 données nécessaires du formulaire
-        $id_categorie = $_POST['id_categorie'];
-        $article      = $_POST['article'];
-        $quantite     = $_POST['quantite'];
-        $unite        = $_POST['unite']; // <--- Cet argument manquait !
+        $id_categorie      = $_POST['id_categorie'];
+        $article           = $_POST['article'];
+        $quantite          = $_POST['quantite'];
+        $unite             = $_POST['unite'];
+        $type_distribution = $_POST['type_distribution'];
 
-        // On appelle la fonction avec les 4 paramètres attendus par le modèle
         $donModel->saveAndDispatch(
             $id_categorie,
             $article,
             $quantite,
-            $unite
+            $unite,
+            $type_distribution
         );
 
         header("Location: index.php?action=dashboard");
+    }
+
+    public function reinitialiser() {
+        $db = (new Database())->getConnection();
+        
+        try {
+            $db->exec("SET FOREIGN_KEY_CHECKS = 0");
+
+            $db->exec("TRUNCATE TABLE bngrc_dispatch");
+            $db->exec("TRUNCATE TABLE bngrc_achats");
+            $db->exec("TRUNCATE TABLE bngrc_dons");
+            $db->exec("UPDATE bngrc_besoins SET quantite_restante = quantite_initiale");
+
+            $db->exec("SET FOREIGN_KEY_CHECKS = 1");
+
+            header("Location: index.php?action=dashboard");
+            exit();
+        } catch (Exception $e) {
+            die("Erreur reset : " . $e->getMessage());
+        }
     }
 }
